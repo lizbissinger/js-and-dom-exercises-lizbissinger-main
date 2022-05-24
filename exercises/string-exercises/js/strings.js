e0d2917c1f0e158cfc32ef6b8fb6f333d40ed998
lizbissinger <105609716+lizbissinger@users.noreply.github.com>
2022-05-24 14:11:38 -0400

    completed caps string

---
+++ exercises/string-exercises/js/strings.js
@@ -0,0 +1,37 @@
+class StringFunctions {
+    /**
+     * @param {string} input
+     * @returns {string}
+     * @example
+     * capitalize('hello world') // 'Hello world'
+     * capitalize('HELLO WORLD') // 'Hello world'
+     */
+    capitalize(input) { 
+
+      str = "hello world";
+
+      input = str.substring(0,1).toUpperCase()+str.substring(1);
+
+      return input; 
+    }
+    /**
+     * Returns a reversed string. Example: "abc" => "cba"
+     * @param {string} input - the string input to be processed 
+     * @returns  {string} - the reversed string
+     * @example
+     * reverse('abc') // 'cba'
+     */
+    reverse(input) { 
+      return input;
+    }
+    /**
+     * Returns an array of the words in a string
+     * @param {string} input - the string input to be processed
+     * @returns {string[]} - an array of words in the string
+     * @example
+     * words('hello world') // ['hello', 'world']
+     */
+    split(input) { 
+      return [input];
+    }
+}
